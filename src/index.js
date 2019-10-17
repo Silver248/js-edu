cf10c8988a672bd98ac6b4fc6bcff33ecc7d5e75
Silver248 <wmanzhela@gmail.com>
2019-10-18 01:59:06 +0300

    feat: add final solution

--- src/index.js
+++ src/index.js
@@ -9,22 +9,13 @@ module.exports = function getTimeForEducation(
     knowsProgramming = true,
     config = {family: 4}
     ) {
-      if ((knowsProgramming = true) && (focus=='family')) {
-        return 200;
-      } else if ((knowsProgramming == true) && (focus=='friends')){
-        return 80;
-      } else if ((knowsProgramming == true) && (focus=='normal_life')) {
-        return 40;
-      } else if ((knowsProgramming == true) && (focus=='profession')){
-        return 27;
-      } else if ((knowsProgramming == true) && (focus=='carrier')){
-        return 20;
-      } else if ((knowsProgramming == true) && (focus=='top_peformance')){
-        return 14;
-      } else {
-        return 44;
-      };
-      let weeks = getTimeForEducation('family', true, Config);
-  console.log(weeks);
+  
+      
+    if (knowsProgramming == false){
+      return Math.ceil((800+500)/config[focus]);
+      
+    } else {
+      return Math.ceil(800/config[focus]);
+    }
 
     } 
